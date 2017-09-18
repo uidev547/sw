3b0b62a711ceba65d08f7c9fbc105e7c9c860bbe
Kiran <akiran547@gmail.com>
2017-09-18 16:59:42 +0530

    updated

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,6 @@ const version = "v1::" //Change if you want to regenerate cache
 const staticCacheName = `${version}static-resources`;
 
 const offlineStuff = [
-    './',
     './scripts/index.js',
     './css/index.css',
     './index.js'
